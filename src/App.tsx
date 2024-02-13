8e1e98609175df6ffadee06ef43a204ff78493f2
BillStephens2022 <two4onebill@yahoo.com>
2024-02-13 10:55:33 -0500

    added a custom button component which accepts props

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Button from "./components/Button";
 import Input from "./components/Input";
 
 function App() {
@@ -5,6 +6,13 @@ function App() {
     <main>
       <Input id="name" label="Your name" type="text" />
       <Input id="age" label="Your age" type="number" />
+      <p>
+        <Button el="button">A Button</Button>
+        
+      </p>
+      <p>
+        <Button el="anchor" href="https://www.google.com">A Link</Button>
+      </p>
     </main>
   );
 }
